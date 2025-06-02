9d959430474f1982da90e8c756c8c57cacf52e7f
Marigi84 <marinagisellecordero@gmail.com>
2025-06-02 00:02:36 -0300

    se modifica código para prueba docker

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 nombre = input("Ingrese su nombre: ")
 apellido = input("Ingrese su apellido: ")
-edad = input("Ingrese su edad: ")
 
-print(f"Hola {nombre} {apellido}, tu edad es {edad} años")
+
+print(f"Hola {nombre} {apellido}")
